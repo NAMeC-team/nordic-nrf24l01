922a25d3a1e4f48fe297ccae0795c3ef64f4e99f
j_delsol_catie <j.delsol@catie.fr>
2019-04-18 16:18:09 +0200

    edit constructor, add members and define callback_attach method

--- nrf24l01/nrf24l01.h
+++ nrf24l01/nrf24l01.h
@@ -19,10 +19,55 @@
 
 namespace sixtron {
 
-class nrf24l01
+class NRF24L01
 {
+	enum class Register : uint8_t {
+		// Operations
+		OP_READ             = 0x00,
+		OP_WRITE            = 0x20,
+		OP_RX               = 0x61,
+		OP_TX               = 0xa0,
+		OP_NOP              = 0xff,
+		// Registers
+		REG_CONFIG          = 0x00,
+		REG_EN_AA           = 0x01,
+		REG_EN_RXADDR       = 0x02,
+		REG_SETUP_AW        = 0x03,
+		REG_SETUP_RETR      = 0x04,
+		REG_RF_CH           = 0x05,
+		REG_RF_SETUP        = 0x06,
+		REG_STATUS          = 0x07,
+		REG_STATUS_ZERO     = 0x80,
+		REG_STATUS_RX_DR    = 0x40,
+		REG_STATUS_TX_DS    = 0x20,
+		REG_OBSERVE_TX      = 0x08,
+		REG_RPD             = 0x09,
+		REG_RX_ADDR_P0      = 0x0a,  // 5 bytes
+		REG_RX_ADDR_P1      = 0x0b,  // 5 bytes
+		REG_RX_ADDR_P2      = 0x0c,
+		REG_RX_ADDR_P3      = 0x0d,
+		REG_RX_ADDR_P4      = 0x0e,
+		REG_RX_ADDR_P5      = 0x0f,
+		REG_TX_ADDR         = 0x10, // 5 bytes
+		REG_RX_PW_P0        = 0x11,
+		REG_RX_PW_P1        = 0x12,
+		REG_RX_PW_P2        = 0x13,
+		REG_RX_PW_P3        = 0x14,
+		REG_RX_PW_P4        = 0x15,
+		REG_RX_PW_P5        = 0x16,
+		REG_FIFO_STATUS     = 0x17,
+		REG_DYNPD           = 0x1c,
+		REG_FEATURE         = 0x1d
+	};
 public:
-    nrf24l01();
+	NRF24L01(SPI *spi, PinName com_ce, PinName irq);
+
+	void attach(Callback<void()> func);
+
+private:
+	SPI *_spi;
+	DigitalOut _com_ce;
+	InterruptIn _irq;
 };
 
 } // namespace sixtron
--- src/nrf24l01.cpp
+++ src/nrf24l01.cpp
@@ -14,14 +14,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#include "mbed.h"
+e
 #include "nrf24l01/nrf24l01.h"
 
 namespace sixtron {
 
-nrf24l01::nrf24l01()
+NRF24L01::NRF24L01(SPI *spi, PinName com_ce, PinName irq):
+		_spi(&spi), _com_ce(com_ce), _irq(irq)
 {
+	_com_ce = 0;
 }
+
+
+void NRF24L01::attach(Callback<void()> func)
+{
+	 if (func) {
+		 _irq.fall(func);
+		 _irq.enable_irq();
+	} else {
+		_irq.fall(NULL);
+		_irq.disable_irq();
+	}
+
+}
+
 
 } // namespace sixtron
 
